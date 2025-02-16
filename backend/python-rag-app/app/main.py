c7d4953a074cd50a40db62ef919d6b86c68aa6b4
vinay khaire <vinaykhaire.06@gmail.com>
2025-02-16 09:22:01 +0530

    unued copde removed

--- backend/python-rag-app/app/main.py
+++ backend/python-rag-app/app/main.py
@@ -2,7 +2,6 @@
 
 from fastapi import FastAPI
 from app.api.documents import router as documents_router
-from app.api.generate import router as generate_router
 from app.api.search import router as search_router
 
 app = FastAPI()
